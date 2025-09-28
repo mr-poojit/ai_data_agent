29d38d5c24436a157b400f5b7c5c970477ca7b1e
mr-poojit <princepoojit123@gmail.com>
2025-09-28 23:40:41 +0530

    Deployment

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://docubot-ai-psi.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
